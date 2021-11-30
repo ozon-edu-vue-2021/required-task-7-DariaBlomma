a7ca0d3c55d49abe73017a6b2e493d335f75c3fb
dkozina <dkozina@srv.rbt>
2021-11-30 09:51:43 +0300

    requred task done

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ const isFriends = document.querySelectorAll('.is-friend');
 const isNotFriends = document.querySelectorAll('.is-not-friend');
 const isPopulars = document.querySelectorAll('.is-popular');
 
-let allFriendsArr = [];
-let allFriendsCounted = [];
-let top3Friends = [];
+const allFriendsArr = [];
 let data = [];
 
 const renderPersonName = (parent, name, id, friends) => {
@@ -36,14 +34,24 @@ const fetchData = async () => {
     }
 };
 
-const renderPeopleList = async () => {
+// * загружает данные, рендерит список контактов, находит и  рендерит топ 3 друзей
+const initFriendsApp = async () => {
     await fetchData();
     data.forEach(item => {
         renderPersonName(contactsList, item.name, item.id, item.friends);
+
         allFriendsArr.push(item.friends[0], item.friends[1], item.friends[2]);
     })
-    console.log('allFriendsArr: ',  allFriendsArr);
-    const result = allFriendsArr.reduce((acc, rec, index) => {
+
+    findTop3Friends();
+};
+
+
+const findTop3Friends = () => {
+    let allFriendsCounted = [];
+    const top3ForSort = [];;
+
+    const repeatedFriendsNumber = allFriendsArr.reduce((acc, rec, index) => {
         // * acc[rec] = rec - число в массиве
         // * { 10: 1} - число массива, его кол-во вхождений
         // * typeof acc[rec] === 'undefined' когда число встречается первый раз и acc[rec] еще не был записан
@@ -53,20 +61,63 @@ const renderPeopleList = async () => {
           : { ...acc, [rec]: 1 };
     }, {});
 
-    for (const [key, value] of Object.entries(result)) { 
+    for (const [key, value] of Object.entries(repeatedFriendsNumber)) { 
         allFriendsCounted.push({ key, value });
     }
-    console.log('result: ', result);
-    allFriendsCounted.sort((prev, next) => next.value - prev.value);
-    allFriendsCounted.forEach((item, index) => {
 
-    })
-    console.log('allFriendsCounted: ', allFriendsCounted);
+    // * сортируем повторяющиеся элементы по числу повторений
+    allFriendsCounted.sort((prev, next) => next.value - prev.value);
+    // * выбираем первичный топ 3
+    const top3Friends = allFriendsCounted.splice(0, 3);
+
+    // * найдем повторяющиеся внутри топ 3
+    top3Friends.forEach((item, index) => {
+        if (top3Friends[index + 1]) {
+            if (item.value === top3Friends[index + 1].value) {
+                top3ForSort.push(item);
+                top3ForSort.push(top3Friends[index + 1]);
+            }
+        }
+    });
+
+    // * найдем повторяющиеся из топ 3 и общем массиве друзей с количестовом. 
+    // * это может быть, если : 
+    // * 1. Все 3 топ 3 одинаковые
+    // * 2. Последний эл-т из топ 3 повторяется в общем массиве allFriendsCounted
+    const repeatedTopFriends = allFriendsCounted.filter(item => item.value === top3Friends[2].value)
+
+    // * соединим повторяющиеся в один массив
+    repeatedTopFriends.forEach(item => {
+        top3ForSort.push(item);
+    });
+
+    // * добавляем имена к топу повторяющихся
+    let repeatedTopFriendsWithNames = top3ForSort.map(item => {
+        const person = data.find(elem => elem.id === parseInt(item.key));
+        return {
+            ...item,
+            name: person.name,
+        }
+
+        ;
+    });
+
+    // * сортируем повторяющиеся по имени и кол-ву повторений
+    repeatedTopFriendsWithNames.sort((prev, next) => {
+        if ( (prev.name < next.name) && (prev.value < next.value )) return -1;
+        if ( (prev.name > next.name) && (prev.value > next.value )) return 1;
+    });
+
+
+    const top3 = repeatedTopFriendsWithNames.splice(0, 3);
+    // * рендерим
+    top3.forEach((item, index) => {
+        isPopulars[index].innerText = item.name
+    });
 };
 
-renderPeopleList();
-
-const openPersonCard = () => {
+// * обработчик событий клика на открытие и закрытие нужного списка
+const togglePersonCard = () => {
     document.addEventListener('click', ({ target }) => {
         const li = target.closest('.person-name');
         if (li) {
@@ -81,14 +132,16 @@ const openPersonCard = () => {
         }
     });
 };
-openPersonCard();
 
+initFriendsApp();
 
+togglePersonCard();
+
+// * находит и рендерит друзей и не друзей
 const findFriends = (friends) => {
     const notFriendsList = [];
-    const friendsNumbers = friends.split(',');
-    console.log('friends: ', friendsNumbers);
-    data.forEach((item, outerIndex) => {
+    const friendsNumbers = friends.split(',');;
+    data.forEach(item => {
         let isFriend = 0;
         friendsNumbers.forEach((number, index) => {
             if (item.id === parseInt(number)) {
@@ -110,8 +163,3 @@ const findFriends = (friends) => {
         });
     })
 };
-
-// const countTopFriends = async () => {
-   
-
-// countTopFriends();
